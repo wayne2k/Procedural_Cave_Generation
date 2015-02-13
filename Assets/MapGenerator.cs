6ef8407f4f00b8e6e5d6a16b9305795c0af58ffe
wayne2k <wayne2k@gmail.com>
2015-02-13 18:12:21 +0200

    Map Smoothing

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -9,6 +9,7 @@ public class MapGenerator : MonoBehaviour
 	public int randomFillPercent = 45;
 	public string seed = "";
 	public bool useRandomSeed;
+	public int mapSmoothing = 5;
 
 	int[,] map;
 
@@ -29,8 +30,12 @@ public class MapGenerator : MonoBehaviour
 	void GenerateMap ()
 	{
 		map = new int[width, height];
-
 		RandomFillMap ();
+
+		for (int i=0; i<mapSmoothing; i++)
+		{
+			SmoothMap ();
+		}
 	}
 
 	void RandomFillMap ()
@@ -46,9 +51,51 @@ public class MapGenerator : MonoBehaviour
 		{
 			for (int y=0; y<height; y++)
 			{
-				map [x, y] = (pseudoRandom.Next (0, 100) < randomFillPercent) ? 1 : 0;
+				if (x == 0 || x == width-1 || y == 0 || y == height-1)
+					map [x, y] = 1;
+				else
+					map [x, y] = (pseudoRandom.Next (0, 100) < randomFillPercent) ? 1 : 0;
 			}
 		}
+	}
+
+	void SmoothMap ()
+	{
+		for (int x=0; x<width; x++) {
+			for (int y=0; y<height; y++) 
+			{
+				int neighbourWallTiles = GetSurroundingWallCount (x, y);
+
+				if (neighbourWallTiles > 4)
+				{
+					map [x, y] = 1;
+				}
+				else if (neighbourWallTiles < 4)
+				{
+					map [x, y] = 0;
+				}
+			}
+		}
+	}
+
+	int GetSurroundingWallCount (int gridX, int gridY)
+	{
+		int wallCount = 0;
+
+		for (int neighbourX=gridX - 1; neighbourX <= gridX + 1; neighbourX++) {
+			for (int neighbourY=gridY - 1; neighbourY <= gridY + 1; neighbourY++) {
+				if (neighbourX >= 0 && neighbourX < width && neighbourY >=0 && neighbourY < height)
+				{
+					if (neighbourX != gridX || neighbourY != gridY)
+						wallCount += map [neighbourX, neighbourY];
+				}
+				else
+				{
+					wallCount++;
+				}
+			}
+		}
+		return wallCount;
 	}
 
 	void OnDrawGizmos ()
